2335048ae9084174bfc7a194aaea31655e6a1e11
Bizcoins <college.cds@gmail.com>
2025-11-20 05:22:35 -0500

    Update PreferencesKeys.kt

--- app/src/main/kotlin/com/pranavpurwar/applock/data/datastore/PreferencesKeys.kt
+++ app/src/main/kotlin/com/pranavpurwar/applock/data/datastore/PreferencesKeys.kt
@@ -1,11 +1,15 @@
 package com.pranavpurwar.applock.data.datastore
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 
 /**
  * Defines all keys used for storing user preferences in DataStore.
  */
 object PreferencesKeys {
-    // This key will store a boolean: true if Fingerprint Only is selected, false otherwise.
+    // Existing key for the PIN (Required for app function)
+    val KEY_PIN = stringPreferencesKey("pin_key")
+
+    // The new key for the biometric toggle
     val FINGERPRINT_ONLY_BIOMETRICS = booleanPreferencesKey("fingerprint_only_biometrics")
 }
